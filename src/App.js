d05be8a083268bd95e9d772e90656b0aad308d69
Eduardo Kuchenbecker <eduardokuchenbecker@hotmail.com>
2020-07-02 21:58:04 -0300

    corrigir requisição login

--- src/App.js
+++ src/App.js
@@ -2,54 +2,30 @@ import React from "react";
 import "./App.css";
 import { Container, Input } from "@material-ui/core";
 import { createHashHistory } from "history";
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 
 export const history = createHashHistory();
 
 function App() {
+  const url = "http://localhost:3003/api/users/login";
 
   const formik = useFormik({
     initialValues: {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
     },
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+
+    onSubmit: (values) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formik),
+      });
     },
   });
-
-
-
-  console.log("teste fora");
-  
-  async function submitForm(formData) {
-    //const url = "http://localhost:3003/api/users/login";
-
-    const form = formData.target;
-
-    let user = {
-      email: form.login,
-      password: form.password,
-    };
-
-    console.log("teste dentro");
-    console.log(user);
-
-    /*const data = await fetch(url, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    });
-
-    if (data) {
-    window.location.href = "dashboard.js";}
-    else {alert("Login ou senha incorretos")}*/
-  }
-
-
 
   return (
     <div className="App">
@@ -71,15 +47,21 @@ function App() {
 
             <span className="App-fields">Senha</span>
 
-            <input name="password" id="password" className={`App-fields loginInput`} type="password" onChange={formik.handleChange} value={formik.values.password}></input>
+            <input
+              name="password"
+              id="password"
+              className={`App-fields loginInput`}
+              type="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            ></input>
 
             <input type="submit" className="loginButton" value="Login" />
-            
           </form>
         </header>
       </Container>
     </div>
   );
-} 
+}
 
 export default App;
